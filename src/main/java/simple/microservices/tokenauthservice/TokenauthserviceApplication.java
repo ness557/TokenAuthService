fd203b90872e9d10df29cfd0d17fc4d2cb3c31e3
ness <ness55779@gmail.com>
2018-06-11 12:47:47 +0300

    Initial commit

---
+++ src/main/java/simple/microservices/tokenauthservice/TokenauthserviceApplication.java
@@ -0,0 +1,12 @@
+package simple.microservices.tokenauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenauthserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenauthserviceApplication.class, args);
+    }
+}
